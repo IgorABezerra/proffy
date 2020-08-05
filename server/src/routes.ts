a8f9c852f6a9451703bd5b3ef0b75dae2669599d
corvoloso <igoralves.b18@gmail.com>
2020-08-04 23:53:14 -0300

    💻 Rest API

---
+++ server/src/routes.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+import ClassesController from './controllers/ClassesController';
+
+const routes = express.Router();
+
+const classesController = new ClassesController();
+
+routes.post('/classes', classesController.create);
+
+export default routes;
